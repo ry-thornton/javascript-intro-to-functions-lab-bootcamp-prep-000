f24238391a1e36c1c6d7bd441deb1fb7f536caf5
ryanthornton628 <ryanthornton628@gmail.com>
2019-04-20 00:05:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,11 @@ function logWhisper(string)
   console.log(string.toLowerCase())
 }
 
-var uppercase === string.toUpperC
-
 function sayHiToGrandma(string)
 {
-  if ( (string) === string)
+  if ( (string) === string.toLowerCase())
   return "I can\'t hear you!"
-  else if ( (string) === uppercase)
+  else if ( (string) === string.toUpperCase())
   return "YES INDEED!"
   else if ( (string) === "I love you, Grandma.")
   return "I love you, too."
