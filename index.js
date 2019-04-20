990dcea41d7d6e9b4c482390a3f4ef6dfa141a45
ryanthornton628 <ryanthornton628@gmail.com>
2019-04-20 00:01:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,5 +26,6 @@ function sayHiToGrandma(string)
   return "I can\'t hear you!"
   else if ( (string) === uppercase)
   return "YES INDEED!"
-  else if ( (string) === "I love you, ")
+  else if ( (string) === "I love you, Grandma.")
+  return "I love you, too."
 }
